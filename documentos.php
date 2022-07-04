5fb87fd94d92170e5368eefbf7b4943636e16a4e
D4N11996 <soporte@sanagustinsjl.edu.pe>
2022-07-04 15:22:29 -0500

    lol

---
+++ documentos.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>I.E.P. San Agustín de Hipona</title>
+    <link rel="icon" href="img/logo.png">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fd7be7c1fb.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/barra.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href='css/agency.css' rel='stylesheet'>
+
+    <!-- Custom styles for windows emergent -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- PAQUETE DE ESTILOS DE SLIDER -->
+    <link href="css/slider.css" rel="stylesheet" type="text/css" />
+
+
+
+  </head>
+
+  <body id="page-top">
+
+
+    <!-- Navigation -->
+    <div class="header ">
+       <div class="container">
+          <div class="header-main">
+             <div class="logo">
+                <a href="index.php"><img src="img/logo-membrete.png" width="230" height="60"/></a>
+             </div>
+
+             <button class=" open-nav-menu" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <strong>Menu</strong>
+                 <i class="fa fa-bars"></i>
+              </button>
+             <div class="menu-overlay">
+             </div>
+             <!-- navigation menu start -->
+             <nav class="nav-menu">
+               <div class="close-nav-menu">
+                  <img src="img/close.svg" alt="close">
+               </div>
+               <ul class="menu">
+                 <li class="menu-item">
+                    <a href="index.php"> <i class="fas fa-home"></i> Inicio</a>
+                 </li>
+                  <li class="menu-item menu-item-has-children" style="margin-top: 21px;">
+
+                     <a href="#" data-toggle="sub-menu"><i class="fas fa-list-alt"></i> Servicios <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="edu-inicial.php">Educación Inicial</a></li>
+                         <li class="menu-item"><a href="edu-primaria.php">Educación Primaria</a></li>
+                         <li class="menu-item"><a href="edu-secundaria.php">Educación Secundaria</a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item menu-item-has-children">
+                     <a href="#" data-toggle="sub-menu"><i class="fas fa-map-marked-alt"></i> <strong>Infraestructura</strong> <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="personal.html">Personal</a></li>
+                         <li class="menu-item"><a href="#">Piscina</a></li>
+                         <li class="menu-item"><a href="#">Laboratorio</a></li>
+                         <li class="menu-item"><a href="#">Movilidad</a></li>
+                         <li class="menu-item"><a href="#">Sala de Computo</a></li>
+                         <li class="menu-item"><a href="#">Cancha deportiva</a></li>
+                         <li class="menu-item"><a href="#">Juego recreativos de Inicial</a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas fa-address-card"></i> <strong>Consultas</strong> </a>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas  fa-folder"></i> <strong>Documentos</strong></a>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas fa-graduation-cap"></i> <strong>Cubibol</strong></a>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas  fa-gears"></i> <strong>Soporte</strong></a>
+                  </li>
+               </ul>
+             </nav>
+             <!-- navigation menu end -->
+          </div>
+       </div>
+    </div>
+<section>
+  <div class="container">
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+<footer>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4">
+            <span class="copyright">Copyright © Centro Radiológico Digital 2018</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a target="_blank" href="https://www.facebook.com/centroradiologicodigital/">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=51916602775">
+                  <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a target="_blank" href="https://www.instagram.com/centroradiologicodigital/">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline quicklinks">
+              <li class="list-inline-item">
+                <a href="#">Política de Privacidad</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Términos y Condiciones</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  </body>
+
+
+
+
+</html>
--- edu-primaria.php
+++ edu-primaria.php
@@ -1,12 +1,155 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
-</head>
-<body>
-<section id="informacion">
+<html lang="en">
+
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>I.E.P. San Agustín de Hipona</title>
+    <link rel="icon" href="img/logo.png">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fd7be7c1fb.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/barra.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href='css/agency.css' rel='stylesheet'>
+
+    <!-- Custom styles for windows emergent -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- PAQUETE DE ESTILOS DE SLIDER -->
+    <link href="css/slider.css" rel="stylesheet" type="text/css" />
+
+
+
+  </head>
+
+  <body id="page-top">
+
+
+    <!-- Navigation -->
+    <div class="header ">
+       <div class="container">
+          <div class="header-main">
+             <div class="logo">
+                <a href="index.php"><img src="img/logo-membrete.png" width="230" height="60"/></a>
+             </div>
+
+             <button class=" open-nav-menu" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <strong>Menu</strong>
+                 <i class="fa fa-bars"></i>
+              </button>
+             <div class="menu-overlay">
+             </div>
+             <!-- navigation menu start -->
+             <nav class="nav-menu">
+               <div class="close-nav-menu">
+                  <img src="img/close.svg" alt="close">
+               </div>
+               <ul class="menu">
+                 <li class="menu-item">
+                    <a href="index.php"> <i class="fas fa-home"></i> Inicio</a>
+                 </li>
+                  <li class="menu-item menu-item-has-children" style="margin-top: 21px;">
+
+                     <a href="#" data-toggle="sub-menu"><i class="fas fa-list-alt"></i> Servicios <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="edu-inicial.php">Educación Inicial</a></li>
+                         <li class="menu-item"><a href="edu-primaria.php">Educación Primaria</a></li>
+                         <li class="menu-item"><a href="edu-secundaria.php">Educación Secundaria</a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item menu-item-has-children">
+                     <a href="#" data-toggle="sub-menu"><i class="fas fa-map-marked-alt"></i> <strong>Infraestructura</strong> <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="personal.html">Personal</a></li>
+                         <li class="menu-item"><a href="#">Piscina</a></li>
+                         <li class="menu-item"><a href="#">Laboratorio</a></li>
+                         <li class="menu-item"><a href="#">Movilidad</a></li>
+                         <li class="menu-item"><a href="#">Sala de Computo</a></li>
+                         <li class="menu-item"><a href="#">Cancha deportiva</a></li>
+                         <li class="menu-item"><a href="#">Juego recreativos de Inicial</a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas fa-address-card"></i> <strong>Consultas</strong> </a>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas  fa-folder"></i> <strong>Documentos</strong></a>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas fa-graduation-cap"></i> <strong>Cubibol</strong></a>
+                  </li>
+                  <li class="menu-item">
+                     <a href="#"><i class="fas  fa-gears"></i> <strong>Soporte</strong></a>
+                  </li>
+               </ul>
+             </nav>
+             <!-- navigation menu end -->
+          </div>
+       </div>
+    </div>
+
+<!-- Inicia bloques de informacion-->
+  <section>
+  <div class="container">
+  <div class="clearfix">
+  <div class="col-md-8 float-md-end mb-3 ms-md-3">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/1.jpg" class="d-block w-100" height= "200px" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/inicial.jpg" class="d-block w-100" height="250px" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.jpg" class="d-block w-100" height="200px" alt="...">
+    </div>
+  </div>
+</div>
+
+  </div>
+  <div class="text-justify">
+     <p>A partir del proceso de individuación iniciado en los primeros años de vida, los niños y las niñas de estas edades HAN LOGRADO DIFERENCIARSE DE LOS DEMÁS, Y EMPIEZAN UN PROCESO DE AFIRMACIÓN DE SU IDENTIDAD, DESDE EL CONOCIMIENTO DE SUS CARACTERÍSTICAS PERSONALES Y LA VALORACIÓN DE SÍ MISMOS.<br></p>
+     <p>En este proceso, continúan desarrollando su autonomía, aprenden a reconocer y expresar con mayor seguridad sus emociones, y a regularlas progresivamente con el acompañamiento del docente. A medida que fortalecen estos aspectos desarrollan sus habilidades sociales, aprenden a convivir con los demás y a cuidar los espacios y recursos comunes que utilizan.<br></p>
+     <p>De igual manera, aprenden a respetar y a construir acuerdos y normas de convivencia. En el proceso de su desarrollo psicomotriz, los niños y las niñas viven su cuerpo a través de la libre exploración y experimentación de sus movimientos, posturas, desplazamientos y juegos, en interacción permanente con su entorno y ambiente.<br></p>
+</div>
+</div>
+</div>
+
+<div class="container text-center">
+  <div class="team-member">
+    <img class="mx-auto" style="width: 100px; height: auto;" class="img-fluid" src="img-sa/graduate-cap.png" alt="">
+  </div>
+</div>
+</section>
+
+<!---Start Informacion--->
+<section id="informacion" style="margin-top: -330px;">
   <div class="container">
   <div class="row justify-content-start">
     <div class="col-md-4">
@@ -34,22 +177,166 @@
      </div>     
     <div class="text-justify">
     <p>
-    A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a few meaningless phrases here to demonstrate how the columns interact here with the floated image.
-  </p>
-
-  <p>
-    As you can see the paragraphs gracefully wrap around the floated image. Now imagine how this would look with some actual content in here, rather than just this boring placeholder text that goes on and on, but actually conveys no tangible information at. It simply takes up space and should not really be read.
-  </p>
-
-  <p>
-    And yet, here you are, still persevering in reading this placeholder text, hoping for some more insights, or some hidden easter egg of content. A joke, perhaps. Unfortunately, there's none of that here.
+   A partir del proceso de individuación iniciado en los primeros años de vida, los niños y las niñas de estas edades HAN LOGRADO DIFERENCIARSE DE LOS DEMÁS, Y EMPIEZAN UN PROCESO DE AFIRMACIÓN DE SU IDENTIDAD, DESDE EL CONOCIMIENTO DE SUS CARACTERÍSTICAS PERSONALES Y LA VALORACIÓN DE SÍ MISMOS.
+En este proceso, continúan desarrollando su autonomía, aprenden a reconocer y expresar con mayor seguridad sus emociones, y a regularlas progresivamente con el acompañamiento del docente. A medida que fortalecen estos aspectos desarrollan sus habilidades sociales, aprenden a convivir con los demás y a cuidar los espacios y recursos comunes que utilizan.
   </p>
 </div>
   </div>
 </div>
+    
 </div>
 </div>
 </section>
 <!---End Informacion--->
-</body>
+
+<!---Start Socializacion--->
+<section id="socializacion" style="margin-top: -290px;">
+  <div class="container">
+<div class="row justify-content-start">
+<div class="col-md-8"> 
+  <div class="clearfix"> 
+    <div class="mx-auto col-md-8 float-md-start mb-3 ms-md-3">
+     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+     </div>   
+    <div class="text-justify">
+    <p>
+     A partir del proceso de individuación iniciado en los primeros años de vida, los niños y las niñas de estas edades HAN LOGRADO DIFERENCIARSE DE LOS DEMÁS, Y EMPIEZAN UN PROCESO DE AFIRMACIÓN DE SU IDENTIDAD, DESDE EL CONOCIMIENTO DE SUS CARACTERÍSTICAS PERSONALES Y LA VALORACIÓN DE SÍ MISMOS.
+En este proceso, continúan desarrollando su autonomía, aprenden a reconocer y expresar con mayor seguridad sus emociones, y a regularlas progresivamente con el acompañamiento del docente. A medida que fortalecen estos aspectos desarrollan sus habilidades sociales, aprenden a convivir con los demás y a cuidar los espacios y recursos comunes que utilizan.
+  </p>
+  </div>
+  </div>
+    </div>
+<div class="col-md-4">
+      <div class="team-member">
+      <img class="mx-auto" style="width: 200px; height: auto;" src="img/que-habla.png" alt="">
+      <h4 style="color: #A66E24;">Socializacion</h4>
+      
+    </div>
+    </div>
+    </div>
+ </div>
+</section>
+<!---End Socializacion--->
+
+<!---Start Utiles--->
+<section id="utiles" style="margin-top: -290px;">
+  <div class="container">
+<div class="row justify-content-end">
+ <div class="col-md-4">
+      <div class="team-member">
+      <img class="mx-auto" style="width: 200px; height: auto;" src="img/mochila.png" alt="">
+      <h4 style="color: #A66E24;">Utiles Escolares</h4>
+    </div>
+    </div>
+  <div class="col-md-8">
+      <div class="clearfix">
+        <div class="mx-auto col-md-8 float-md-end mb-3 ms-md-3">
+     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+     </div>     
+    <div class="text-justify">
+    <p>
+     A partir del proceso de individuación iniciado en los primeros años de vida, los niños y las niñas de estas edades HAN LOGRADO DIFERENCIARSE DE LOS DEMÁS, Y EMPIEZAN UN PROCESO DE AFIRMACIÓN DE SU IDENTIDAD, DESDE EL CONOCIMIENTO DE SUS CARACTERÍSTICAS PERSONALES Y LA VALORACIÓN DE SÍ MISMOS.
+En este proceso, continúan desarrollando su autonomía, aprenden a reconocer y expresar con mayor seguridad sus emociones, y a regularlas progresivamente con el acompañamiento del docente. A medida que fortalecen estos aspectos desarrollan sus habilidades sociales, aprenden a convivir con los demás y a cuidar los espacios y recursos comunes que utilizan.aster egg of content. A joke, perhaps. Unfortunately, there's none of that here.
+  </p>
+</div>
+  </div>
+</div>
+    </div>
+</div>
+</section>
+<!---End Utiles--->
+
+<div class="container text-center" style="margin-top: -150px;">
+  <div class="team-member">
+    <img class="mx-auto" style="width: 100px; height: auto;" class="img-fluid" src="img-sa/graduate-cap.png" alt="">
+  </div>
+</div>
+
+<!--Start malla curricural -->
+<section id="malla" style="margin-top: -200px;">
+  <div class="container text-center">
+  <div class="team-member">
+    <img class="mx-auto" style="width: 800px; height: auto;" class="img-fluid" src="img-sa/malla.png" alt="">
+  </div>
+</div>
+</section>
+<!--End malla curricural -->
+<footer>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4">
+            <span class="copyright">Copyright © Centro Radiológico Digital 2018</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a target="_blank" href="https://www.facebook.com/centroradiologicodigital/">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=51916602775">
+                  <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a target="_blank" href="https://www.instagram.com/centroradiologicodigital/">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline quicklinks">
+              <li class="list-inline-item">
+                <a href="#">Política de Privacidad</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Términos y Condiciones</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  </body>
+
+
+
+
 </html>
