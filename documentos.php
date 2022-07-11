e5ecead49fb2ba12d0071d653a21186aff6796b6
D4N11996 <soporte@sanagustinsjl.edu.pe>
2022-07-11 14:39:53 -0500

    pdf docs
    e

--- documentos.php
+++ documentos.php
@@ -139,7 +139,14 @@
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="docs/cubicol-braco.pdf">
+              <img src="img-sa/pdf.png" style="width: 60px;">
+              Informacion de los docentes
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
