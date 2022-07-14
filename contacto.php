8e7bc5f244d1c91d99d063f6276c9780af9127c5
D4N11996 <soporte@sanagustinsjl.edu.pe>
2022-07-14 14:57:41 -0500

    contact

---
+++ contacto.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>I.E.P. San Agustín de Hipona</title>
+    <link rel="icon" href="img/logo.png">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/fd7be7c1fb.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/barra.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href='css/agency.css' rel='stylesheet'>
+
+    <!-- Custom styles for windows emergent -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- PAQUETE DE ESTILOS DE SLIDER -->
+    <link href="css/slider.css" rel="stylesheet" type="text/css" />
+
+    <!-- PAQUETE DE fontawesome (iconos)-->
+    <link href="fontawesome/css/all.css" rel="stylesheet" type="text/css" />
+    <link href="fontawesome/css/solid.css" rel="stylesheet" type="text/css" />
+
+
+
+  </head>
+
+  <body id="page-top">
+
+
+    <!-- Navigation -->
+    <div class="header ">
+       <div class="container">
+          <div class="header-main">
+             <div class="logo">
+                <a href="index.php"><img src="img/logo-membrete.png" width="230" height="60"/></a>
+             </div>
+
+             <button class=" open-nav-menu" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <strong>Menu</strong>
+                 <i class="fa fa-bars"></i>
+              </button>
+             <div class="menu-overlay">
+             </div>
+             <!-- navigation menu start -->
+             <nav class="nav-menu">
+               <div class="close-nav-menu">
+                  <img src="img/close.svg" alt="close">
+               </div>
+               <ul class="menu">
+                 <li class="menu-item">
+                    <a href="index.php"> <i class="fas fa-home"></i> Inicio</a>
+                 </li>
+                  <li class="menu-item menu-item-has-children" style="margin-top: 21px;">
+
+                     <a href="#" data-toggle="sub-menu"><i class="fas fa-list-alt"></i> Servicios <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="edu-inicial.php">Educación Inicial</a></li>
+                         <li class="menu-item"><a href="edu-primaria.php">Educación Primaria</a></li>
+                         <li class="menu-item"><a href="edu-secundaria.php">Educación Secundaria</a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item menu-item-has-children">
+                     <a href="#" data-toggle="sub-menu"><i class="fas fa-map-marked-alt"></i> <strong>Infraestructura</strong> <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="personal.html">Personal</a></li>
+                         <li class="menu-item"><a href="piscina.html">Piscina</a></li>
+                         <li class="menu-item"><a href="#">Laboratorio</a></li>
+                         <li class="menu-item"><a href="#">Movilidad</a></li>
+                         <li class="menu-item"><a href="#">Sala de Computo</a></li>
+                         <li class="menu-item"><a href="#">Cancha deportiva</a></li>
+                         <li class="menu-item"><a href="#">Juego recreativos de Inicial</a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item menu-item-has-children">
+                     <a href="#" data-toggle="sub-menu"><i class="fa-solid fa-address-book"></i> <strong>Contacto</strong> <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href=""><strong>Consultas</strong></a></li>
+                         <li class="menu-item"><a href="#"><strong>Citas</strong></a></li>
+                     </ul>
+
+                  </li>
+                  <li class="menu-item menu-item-has-children">
+                     <a href="#" data-toggle="sub-menu"><i class="fa  fa-globe"></i> <strong>Comunidad</strong> <i class="plus"></i></a>
+                     <ul class="sub-menu">
+                         <li class="menu-item"><a href="#"><strong>Noticias</strong></a></li>
+                         <li class="menu-item"><a href="#"><strong>Testimonios</strong></a></li>
+                         <li class="menu-item"><a href=""><strong>Soporte</strong></a></li>
+                         <li class="menu-item"><a href="documentos.php"><strong>Documentos</strong></a></li>
+                     </ul>
+                  </li>
+                  <li class="menu-item">
+                     <a href="https://sanagustinsjl.cubicol.pe/"><i class="fas fa-graduation-cap"></i> <strong>Cubibol</strong></a>
+                  </li>
+               </ul>
+             </nav>
+             <!-- navigation menu end -->
+          </div>
+       </div>
+    </div>
+
+<!--Form contact-->
+<section id="Contact">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-4">
+        holi
+      </div>
+      <div class="col-4">
+        hola
+      </div>
+    </div>
+    
+  </div>
+</section>
+
+<footer>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4">
+            <span class="copyright">Copyright © Centro Radiológico Digital 2018</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a target="_blank" href="https://www.facebook.com/centroradiologicodigital/">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=51916602775">
+                  <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a target="_blank" href="https://www.instagram.com/centroradiologicodigital/">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline quicklinks">
+              <li class="list-inline-item">
+                <a href="#">Política de Privacidad</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Términos y Condiciones</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  </body>
+
+
+
+
+</html>
